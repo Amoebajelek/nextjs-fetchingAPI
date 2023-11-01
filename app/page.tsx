f5c3acdc0c243ddd1fea1d74c5f12aafb5a33926
Naufal Abdussyakur <76672769+Amoebajelek@users.noreply.github.com>
2023-11-02 00:48:59 +0700

    Change folder name

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex flex-col max-w-4xl gap-7 p-12 text-center items-center mx-auto">
       <h1 className="text-2xl font-bold">Learning Fetching Public API</h1>
-      <Link href='/posts' className='flex items-center gap-2'>Go to Posts <IoIosArrowRoundForward size={24} /></Link>
+      <Link href='/users' className='flex items-center gap-2'>Go to Users <IoIosArrowRoundForward size={24} /></Link>
       <Link href='/updates' className='flex items-center gap-2'>Go to Updates <IoIosArrowRoundForward size={24} /></Link>
     </main>
   )
